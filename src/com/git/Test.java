4969fec79138e51d293a576de8d2f827b508c03c
Infopine <Infopine@DESKTOP-T77F6MG>
2021-01-11 14:29:34 -0800

    committing

--- src/com/git/Test.java
+++ src/com/git/Test.java
@@ -5,8 +5,10 @@ import java.util.Date;
 public class Test {
 
 	public static void main(String[] args) {
+		//printing Date
 		Date d=new Date();
 		System.out.println(d.toString());
+		System.out.println(d.getDay());
 
 	}
 
